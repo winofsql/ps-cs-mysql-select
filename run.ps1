9cd31b45a9214c923db6f1885eb29ef3d2eec672
sworc <lightbox@nifty.com>
2021-07-16 16:31:53 +0900

    Add files via upload

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 Add-Type -path ".\Program.cs" `
-	-ReferencedAssemblies System.Data `
-	-OutputAssembly pg.exe `
-	-OutputType ConsoleApplication
+	-ReferencedAssemblies System.Data
+
+[Program]::Main()
 
 Read-Host "‰½‚©ƒL[‚ğ‰Ÿ‚µ‚Ä‚­‚¾‚³‚¢"
